9bd2c5eff359f6e58eb266f878b85aedc4677c8f
Alexey <peretsmobil@gmail.com>
2019-07-13 01:26:27 +0300

    fix small bug

--- migrations/4_deploy_mixer.js
+++ migrations/4_deploy_mixer.js
@@ -8,7 +8,7 @@ module.exports = function(deployer) {
   return deployer.then(async () => {
     const { MERKLE_TREE_HEIGHT, AMOUNT, EMPTY_ELEMENT } = process.env
     const verifier = await Verifier.deployed()
-    const miMC = MiMC.deployed()
+    const miMC = await MiMC.deployed()
     await Mixer.link(MiMC, miMC.address)
     await deployer.deploy(Mixer, verifier.address, AMOUNT, MERKLE_TREE_HEIGHT, EMPTY_ELEMENT)
   })
