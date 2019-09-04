ec30e2d357c5995c6d942703a708e9b1849784fc
Drygin <drygin@smartdec.ru>
2019-09-04 13:31:10 +0300

    fix merkle tree capacity

--- contracts/MerkleTreeWithHistory.sol
+++ contracts/MerkleTreeWithHistory.sol
@@ -58,7 +58,7 @@ contract MerkleTreeWithHistory {
 
   function _insert(uint256 leaf) internal {
     uint32 current_index = next_index;
-    require(current_index != 2**(levels - 1), "Merkle tree is full");
+    require(current_index != 2**levels, "Merkle tree is full");
     next_index += 1;
     uint256 current_level_hash = leaf;
     uint256 left;
